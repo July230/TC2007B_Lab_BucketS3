7ab6c50741a38119878dcb50205c00354a4c4f52
Ian Julián Estrada Castro <A01352823@tec.mx>
2024-10-26 13:39:55 -0600

    Archivos index js y html para raiz del proyecto y formaulario

---
+++ index.js
@@ -0,0 +1,22 @@
+// Archivo index.js, la raiz del proyecto y para los valores generales
+
+const express = require("express");
+
+const multer = require("multer");
+
+var path = require("path");
+
+const app = express();
+
+const port = 5000;
+
+const log = console.log;
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
+});
+
+app.listen(port, () => {
+  //server starts listening for any attempts from a client to connect at port: {port}
+  console.log(`Now listening on port ${port}`);
+});
