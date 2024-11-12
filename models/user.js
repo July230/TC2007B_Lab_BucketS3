e17517259c5049bc6165b8c4d6ad6ba8235d8a1f
Ian Julián Estrada Castro <A01352823@tec.mx>
2024-11-12 17:38:18 -0600

    modelo user.js

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true},
+    age: { type: Number, required: true},
+})
+
+// Agregar un metodo para devolver si el usuario es un adulto al esquema de usuario
+userSchema.methods.isAdult = function () {
+    return this.age >= 18
+}
+
+const userModel = mongoose("User", userSchema);
+module.exports = userModel;
