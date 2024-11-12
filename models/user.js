bc45a7556abf59408e93395318a44ea49f420323
Ian Julián Estrada Castro <A01352823@tec.mx>
2024-11-12 17:51:57 -0600

    pruebas con jest

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true},
     age: { type: Number, required: true},
@@ -9,5 +10,5 @@ userSchema.methods.isAdult = function () {
     return this.age >= 18
 }
 
-const userModel = mongoose("User", userSchema);
+const userModel = mongoose.model("User", userSchema);
 module.exports = userModel;
---
+++ tests/unit/user.test.js
@@ -0,0 +1,8 @@
+const userModel = require("../../models/user");
+
+describe("User Model Test", () => {
+    it("Should be able to return whether the user is adult", () => {
+        const user = new userModel({username: "John Doe", age:20});
+        expect(user.isAdult()).toBeTruthy();
+    });
+});
