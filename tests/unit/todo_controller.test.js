d14a0871ab6eb9ed490d467aa632a2879de9d45a
Ian Julián Estrada Castro <A01352823@tec.mx>
2024-11-12 18:57:40 -0600

    Primer test para todo_controller

--- tests/unit/todo_controller.test.js
+++ tests/unit/todo_controller.test.js
@@ -2,3 +2,24 @@ const todoModel = require("../../models/todo_model");
 const todoController = require("../../controllers/todo_controller");
 const httpMocks = require("node-mocks-http");
 const newTask = require("../mockups/todo_mockup.json");
+const { beforeEach } = require("mocha");
+const { expect } = require("chai");
+
+todoModel.create = jest.fn()
+
+let req, res, next;
+beforeEach(() => {
+    res = httpMocks.createResponse();
+    req = httpMocks.createRequest();
+    next = null;
+})
+
+describe("Test Create method exists in controller", () => {
+    it("Should have a create method", () => {
+        expect(typeof todoController.todoCreate).toBe("function");
+    });
+
+    it("Should call todoModel.create", () => {
+        expect(typeof todoController.todoCreate).toBe("function");
+    });
+})
