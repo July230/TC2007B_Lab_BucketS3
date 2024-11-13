9b3f31c27630b6402b564d2b4b739ea271e865ad
Ian Julián Estrada Castro <A01352823@tec.mx>
2024-11-12 18:44:58 -0600

    instalar node-mockup-http, preparar todo_controller.test.js

---
+++ tests/unit/todo_controller.test.js
@@ -0,0 +1,4 @@
+const todoModel = require("../../models/todo_model");
+const todoController = require("../../controllers/todo_controller");
+const httpMocks = require("node-mocks-http");
+const newTask = require("../mockups/todo_mockup.json");
